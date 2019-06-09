a52fb5fa6c3f41a4a69787b6bb230ae61534abb1
liyuanzhe-cn <liyuanzhe0@gmail.com>
2019-06-10 01:57:52 +0800

    c

--- my-blog-pro-version/config.js
+++ my-blog-pro-version/config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var globalConfig = {};
 var conf = fs.readFileSync('./server.conf');
-configArr = conf.toString().split('\r\n');
+configArr = conf.toString().split('\n');
 
 for (var i = 0; i < configArr.length; i++) {
     globalConfig[configArr[i].split('=')[0].trim()] = configArr[i].split('=')[1].trim();
